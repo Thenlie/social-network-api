839284c5464c11347e7e62a7f5039b8cfd554d94
Leithen <leithen113@gmail.com>
2022-01-10 17:23:57 -0700

    Created user and thought models

---
+++ models/Thought.js
@@ -0,0 +1,47 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //format date!
+    }
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //format date!
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema]
+});
+
+//create reaction count virtual
+
+const Thought = model('thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [isEmail, 'invalid email!']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+//create friend count virtual
+
+const User = model('user', UserSchema);
+
+module.exports = User;
